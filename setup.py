ac26678d1f55c4377e2c2d6b40bbf3e8bb7b6340
razibdash <avrorazib@gmail.com>
2025-07-17 01:07:39 +0600

    expriment done

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Metropolitan University ChatBot',
+    version= '0.0.1',
+    author= 'Razib Dash',
+    author_email= 'avrorazib@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
